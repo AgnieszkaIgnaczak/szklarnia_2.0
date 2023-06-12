85ace2f3f7abfffd2cdf73b8b3e989f2915aa7d1
Agnieszka Ignaczak <agni.ignaczak@gmail.com>
2023-06-12 20:03:22 +0200

    Initial commit

---
+++ src/main/java/com/szklarnia/szklarnia/SzklarniaApplication.java
@@ -0,0 +1,13 @@
+package com.szklarnia.szklarnia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzklarniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzklarniaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/szklarnia/szklarnia/SzklarniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szklarnia.szklarnia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzklarniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
