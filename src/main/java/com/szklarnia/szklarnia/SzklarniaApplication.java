6024665bc918ecf2eab3b4e6443ef66579108a89
Agnieszka Ignaczak <agni.ignaczak@gmail.com>
2023-06-12 20:57:26 +0200

    initial controller HelloController added to program, it displays string on endpoint localhost:8080/hello

--- src/main/java/com/szklarnia/szklarnia/SzklarniaApplication.java
+++ src/main/java/com/szklarnia/szklarnia/SzklarniaApplication.java
@@ -3,6 +3,7 @@ package com.szklarnia.szklarnia;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//nie może być w pakiecie, bo Maven pomija pliki, aplikacja musi być w rootcie
 @SpringBootApplication
 public class SzklarniaApplication {
 
